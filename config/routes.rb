10f981d0773794366ad1ae790b620d55640a3552
Giovani Ricardo <giovani.amaral@fastgondolas.com.br>
2022-09-20 12:53:58 -0300

    Adicionado metodos set e require_authorization nos models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :usuarios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :funcionarios
+  resources :depesas
+  resources :categorias_depesas
+
 end
